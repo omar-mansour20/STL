baad1090b9c19e4446ed3d998d30bbd13dcc8073
Omar Mansour <123955248+omar-mansour20@users.noreply.github.com>
2023-04-17 14:24:56 +0200

    Delete Piar.cpp

--- Piar.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-    /*
-    - Deque (Double-ended queue) is a sequence container.
-    - Deque is a special case of queue where insertion and deletion operations are possible at both the ends.
-     */
